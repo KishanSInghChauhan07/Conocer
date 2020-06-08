a67ee6978298da2b9affa807abbe5920ebeffee0
kishansinghchauhan <kishancoer7071@gmail.com>
2020-06-08 16:14:22 +0530

    [Kishan] | Add Signup Component

--- src/Components/Sign In/SignIn.Component.js
+++ src/Components/Sign In/SignIn.Component.js
@@ -4,7 +4,6 @@ import {TextField,Card} from '@material-ui/core';
 
 const SignIn = () =>{
     return(
-        <div>
         <Card className='text-center' style={{maxWidth:'40%',margin:'10% auto',padding:'50px 30px',boxShadow:'1px 1px 5px 5px lightgrey'}}>
             <h4 style={{margin:'10px 0 20px 0',fontWeight:'500'}}>Instagram</h4>
             <form className="d-flex flex-column align-items-center" noValidate autoComplete="off">
@@ -14,9 +13,7 @@ const SignIn = () =>{
             <h6>Create an Account
             <Link className='ml-2' to="/SignUp">SignUp</Link>
             </h6>
-
         </Card>
-        </div>
     )
 }
 
--- src/Components/Sign Up/SignUp.Component.js
+++ src/Components/Sign Up/SignUp.Component.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {TextField,Card} from '@material-ui/core';
 
 const SignUp = () =>{
     return(
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <Card className='text-center' style={{maxWidth:'40%',margin:'8% auto',padding:'50px 30px',boxShadow:'1px 1px 5px 5px lightgrey'}}>
+            <h4 style={{margin:'10px 0 20px 0',fontWeight:'500'}}>Instagram</h4>
+            <form className="d-flex flex-column align-items-center" noValidate autoComplete="off">
+                <TextField label="Name"  className="w-75" style={{margin:'25px 0'}} variant="outlined" />
+                <TextField label="Email"  className="w-75" style={{margin:'25px 0'}} variant="outlined" />
+                <TextField label="Password" className="w-75" variant="outlined" style={{margin:'25px 0 40px 0'}}/>  
+            </form>
+            <h6>Already have an Account
+            <Link className='ml-2' to="/SignIn">SignIn</Link>
+            </h6>
+        </Card>
     )
 }
 

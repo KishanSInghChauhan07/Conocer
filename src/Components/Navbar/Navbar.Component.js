717dc68ff1e296753d193720d9e93cffa9523cf1
kishansinghchauhan <kishancoer7071@gmail.com>
2020-06-15 20:18:05 +0530

    [Kishan] | ++

--- src/Components/Navbar/Navbar.Component.js
+++ src/Components/Navbar/Navbar.Component.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import {AppBar,Toolbar} from '@material-ui/core';
-import {Link} from 'react-router-dom'
+import {AppBar,Toolbar,Modal,Backdrop,Card,TextField,IconButton} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+
+const useStyles = makeStyles(() => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  }));
 
 const Navbar = () =>{
+    const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+    const [title, setTitle] = React.useState("");
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return(
         <div>
             <AppBar position="fixed" className="bg-insta">
@@ -14,6 +37,30 @@ const Navbar = () =>{
                         <Link to="/SignUp" className='text-white p-4'>SignUp</Link>
                         <Link to="/SignIn" className='text-white p-4'>SignIn</Link>
                         <Link to="/Profile" className='text-white p-4'>Profile</Link>
+                        <Link onClick={handleOpen} className='text-white p-4'>Post</Link>
+                        {/* <button type="button" onClick={handleOpen}>
+                          react-transition-group
+                        </button> */}
+                        <Modal
+                            className={classes.modal}
+                            open={open}
+                            onClose={handleClose}
+                            closeAfterTransition
+                            BackdropComponent={Backdrop}
+                            BackdropProps={{
+                                timeout: 500,
+                            }}
+                        >
+                            <Card in={open} style={{height:'40%',width:'30%'}} className="text-center">
+                            <TextField label="Caption"  className="w-75" style={{margin:'25px 0'}} variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)} />
+                            <input accept="image/*" style={{display:'none'}} id="icon-button-file" type="file" />
+                            <label htmlFor="icon-button-file">
+                              <IconButton color="primary" aria-label="upload picture" component="span">
+                                <PhotoCamera />
+                              </IconButton>
+                            </label>
+                            </Card>
+                        </Modal>
                     </div>
                 </Toolbar>
             </AppBar>
--- src/Components/Profile/MyPhotos.Component.js
+++ src/Components/Profile/MyPhotos.Component.js
@@ -4,7 +4,7 @@ import {Paper,Tab,Tabs} from '@material-ui/core';
 
 const MyPhotos = () =>{
     return( 
-        <Card className="d-flex flex-column shadow-sm" style={{maxWidth:"80%",margin:'0 auto'}}>
+        <Card className="d-flex flex-column shadow-sm" style={{maxWidth:"80%",margin:'0 auto 30px auto'}}>
             <Paper>
               <Tabs
                 indicatorColor="primary"
@@ -17,13 +17,13 @@ const MyPhotos = () =>{
                 <Tab label="Tagged" />
               </Tabs>
             </Paper>
-            <div className="row pl-3 pr-3 pt-2 pb-2">
-                <img className="col-12 col-sm-3" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
-                <img className="col-sm-3 col-12" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
-                <img className="col-sm-3 col-12" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
-                <img className="col-sm-3 col-12" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
-                <img className="col-sm-3 col-12" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
-                <img className="col-sm-3 col-12" height='300' width='300' style={{margin:'10px 0'}} src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+            <div className="row m-2">
+                <img className="col-12 col-sm-3 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+                <img className="col-sm-3 col-12 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+                <img className="col-sm-3 col-12 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+                <img className="col-sm-3 col-12 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+                <img className="col-sm-3 col-12 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
+                <img className="col-sm-3 col-12 p-0 border" height='300' width='300' src='https://images.unsplash.com/photo-1591690497001-3316502441ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='mt=photos'/>
 
             </div>
         </Card>
